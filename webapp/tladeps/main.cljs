4b109e504b4514eb9767e157c218075cab1fb58d
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-04-15 23:00:25 -0400

    promesa and UI

--- webapp/tladeps/main.cljs
+++ webapp/tladeps/main.cljs
@@ -1,15 +1,52 @@
 (ns tladeps.main
   (:require
+   [clojure.string :as str]
    [rum.core :as rum]
-   ["react-dom/client" :as dom-client]))
+   ["react-dom/client" :as dom-client]
+   [promesa.core :as p :include-macros true]
+   [cljs.pprint :as pp]))
+
+(defn grid-template-areas
+  [grid]
+  {:style
+   {:grid-template-areas
+    (->> grid
+         (mapv (fn [row]
+                 (pr-str (str/join " " (map name row)))))
+         (str/join " "))}})
+
+(defn grid-area
+  [identifier]
+  {:style
+   {:grid-area identifier}})
+
+(defn style
+  [attrs m]
+  (update attrs :style merge m))
 
 (rum/defc app
   []
-  [:div {:class "label"} "abc"])
+  [:<>
+   [:.navbar.bg-base-100
+    [:a {:class "btn btn-ghost normal-case text-xl"}
+     "TLA Deps"]]
+   [:.grid.gap-4.p-24 #_(-> (grid-template-areas [[:search]
+                                                [:a]])
+                            (style {:grid-template-columns "1fr 5fr"}))
+    [:input.input.input-bordered.w-full.max-w-xs {:placeholder "Search..."}]
+    (repeat 5 [:div "Z"])]])
 
 (defonce root
   (.. dom-client
       (createRoot (.getElementById js/document "app"))))
+
+(comment
+
+  (p/let [res (js/fetch "https://clojars.org/search?q=tla&format=json")
+          body (.json res)]
+    (pp/pprint (js->clj body :keywordize-keys true)))
+
+  ())
 
 (defn- app!
   []
@@ -26,3 +63,7 @@
   []
   (app!)
   (js/console.log "Loaded"))
+
+;; TODO:
+;; - [ ] Fetch the deps
+;; - [ ] Present the deps
