3e3c3ba712b6e7ec3ce33e711943941c18cac1e4
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-04-22 12:13:09 -0400

    test

--- src/tladeps/main.clj
+++ src/tladeps/main.clj
@@ -1,34 +1,10 @@
 (ns tladeps.main
   (:gen-class)
   (:require
-   [clojure.java.data.builder :as builder]
    [tladeps.infra])
   (:import
-   (com.pulumi Pulumi)
-   (com.pulumi.core Output)
-   (java.util.function Consumer)))
+   (com.pulumi Pulumi)))
 
 (defn -main
   [& _args]
-  (Pulumi/run
-    (reify Consumer
-      (accept [_ ctx]
-        (tladeps.infra/infra-handler ctx)))))
-
-(comment
-
-  (builder/to-java
-   DomainArgs {:name "ss"
-               :ipAddress "192.168.10.10"}
-   {:builder-fn "builder"})
-
-  (builder/to-java
-   java.util.Locale {:language "fr" :region "EG"})
-
-  (-> (DomainArgs/builder)
-      (.name "ss")
-      (.ipAddress "fff"))
-
-  (clojure.java.data/*to-java-object-missing-setter* DomainArgs)
-
-  ())
+  (Pulumi/run (tladeps.infra/make-consumer)))
