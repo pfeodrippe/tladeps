71b17874e0dd1f771e79a9c7f98decad5f0313c6
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-04-27 23:47:36 -0400

    clj deploy working!!

--- backend/hello.clj
+++ backend/hello.clj
@@ -5,7 +5,7 @@
    [clojure.data.json :as json]))
 
 (gen-class
- :name eita.MyHello
+ :name tladeps.hello
  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]
  :main false
  :prefix "hello-")
@@ -13,9 +13,10 @@
 (defn hello-handleRequest
   #_[{:keys [name] :or {name "Blambda"} :as event} context]
   [_ is os context]
-  (let [logger (.logger context)]
+  (let [logger (.getLogger context)]
     (with-open [is is
                 os os]
+      (.log logger (slurp is))
       (.log logger (pr-str {:msg "Invoked with event",
                             :data {:event "ssss.."}}))
       (with-open [input (io/input-stream (.getBytes
@@ -23,5 +24,3 @@
                                            {:statusCode 200
                                             :body (json/write-str {:hi "TRRRR"})})))]
         (io/copy input os)))))
-
-#_(bean com.amazonaws.services.lambda.runtime.RequestStreamHandler)
